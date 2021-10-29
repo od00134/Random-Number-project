5101b94252c30ca8f4524afde636ff64acbe7cde
od00134 <od00134@tkh.edu.eg>
2021-10-29 18:28:45 +0200

    starting application

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-def lcg(modulus: int, a: int, c: int, seed: int) :
+import time
+M = 2 ** 16 + 1
+A = 75
+C = 74
+
+
+def lcg(modulus, a, c, seed):
     while True:
         seed = (a * seed + c) % modulus
         yield seed
 
 
 if __name__ == '__main__':
-    start = input("Please enter beginig of interval:\n")
-    start = int(start)
-
-    end = input("Please enter end of interval:\n")
-    end = int(end)
-
-    seed = input("Please enter seed of interval:\n")
+    # start = input("Please enter beginig of interval:\n")
+    # start = int(start)
+    #
+    # end = input("Please enter end of interval:\n")
+    # end = int(end)
+    #
+    seed = int(time.time())
     seed = int(seed)
-
-    print(f"start {start}")
-    print(f"end {end}")
-    print(f"seed {seed}")
-
+    #
+    # print(f"start {start}")
+    # print(f"end {end}")
+    # print(f"seed {seed}")
+    generator = lcg(M, C, A, seed)
+    for i in range(5):
+        print(next(generator))
