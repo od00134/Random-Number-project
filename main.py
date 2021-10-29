38a62f53be8fbb8b7adda0b62ae3e15ddae21e70
od00134 <od00134@tkh.edu.eg>
2021-10-29 18:49:34 +0200

    applying range to random number

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+
 M = 2 ** 16 + 1
 A = 75
 C = 74
@@ -10,19 +11,24 @@ def lcg(modulus, a, c, seed):
         yield seed
 
 
+def cut_to_range(start, end, generator, m):
+   return round((end - start) * (next(generator) / m) + start)
+
+
 if __name__ == '__main__':
-    # start = input("Please enter beginig of interval:\n")
-    # start = int(start)
-    #
-    # end = input("Please enter end of interval:\n")
-    # end = int(end)
-    #
+    start = input("Please enter beginig of interval:\n")
+    start = int(start)
+
+    end = input("Please enter end of interval:\n")
+    end = int(end)
+
+    count = input("Please enter count of interval:\n")
+    count = int(count)
+
     seed = int(time.time())
     seed = int(seed)
-    #
-    # print(f"start {start}")
-    # print(f"end {end}")
-    # print(f"seed {seed}")
+
     generator = lcg(M, C, A, seed)
-    for i in range(5):
-        print(next(generator))
+
+    for i in range(count):
+        print(cut_to_range(start, end, generator, M))
